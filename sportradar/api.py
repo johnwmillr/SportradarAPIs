58e18577d2ee3971b6880de93369dda1cf450b00
johnwmillr <john.w.millr@gmail.com>
2018-06-22 22:08:03 -0500

    Add additional endpoints for soccer API

--- sportradar/api.py
+++ sportradar/api.py
@@ -2,61 +2,153 @@
 # Copyright 2018 John W. Miller
 # See LICENSE for details.
 
+"""
+API details and documentation: https://developer.sportradar.com/io-docs
+"""
+
 import requests
+import time
 
 class API(object):
-	"""
-	Sportradar API
+    """Sportradar API
 
-	Currently only supports the "Soccer INTL Trial v3" API
-	Will be generalized to support multiple APIs from Sportradar (e.g. NFL, NBA, etc.)
+    *Currently only supports the "Soccer INTL Trial v3" API*
+    *Will be generalized to support multiple APIs from Sportradar
+    (e.g. NFL, NBA, etc.)*
+    """
 
-	"""	
+    # Create a persistent requests connection
+    session = requests.Session()
+    session.headers = {'application': 'PythonAPI'}
 
-	session = requests.Session()
-	session.headers = {'application': "PythonAPI"}
+    def __init__(self, api_key, format_='json', language='en', timeout=5, sleep_time=1.5):
+        """ Sportradar API Constructor
 
-	def __init__(self, api_key, format='json', timeout=5, language='en'):
-		self._auth = {'api_key': api_key}
-		self._LANGUAGE = language
-		self._BASE_URL = 'https://api.sportradar.us/soccer-t3/intl/' + self._LANGUAGE + '/'
-		self._FORMAT = "." + format.strip(".")
-		self.timeout = timeout						
-		""" API instance Constructor
+        :param api_key: key provided by Sportradar, specific to the sport's API
+        :param format_: response format to request from the API (json, xml)
+        :param language: language of the response data
+        :param timeout: time before quitting on response (seconds)
+        :param sleep_time: time to wait between requests, (free min is 1 second)
+        """
 
-		:param auth: API key provided by Sportradar
-		:param format: response format to request from the API (json, xml)
-		:param timeout: timeout for response (seconds)
-		:param language: language of the response data
-		"""
+        self.api_key = {'api_key': api_key}
+        self._LANGUAGE = language
+        self.api_root = 'https://api.sportradar.us/soccer-t3/intl/' + self._LANGUAGE + '/'
+        self.FORMAT = "." + format_.strip(".")
+        self.timeout = timeout
+        self._sleep_time = sleep_time
 
-	def _make_request(self, path, method='GET'):
-		"""Make a GET or POST request to the API"""
-		full_uri = self._BASE_URL + path + self._FORMAT	
-		response = self.session.request(method,
-										full_uri,
-										timeout=self.timeout,
-										params=self._auth)		
-		response.raise_for_status() # Raise error for bad status
-		return response	
+    def _make_request(self, path, method='GET'):
+        """Make a GET or POST request to the API"""
+        time.sleep(self._sleep_time) # Rate limiting
+        full_uri = self.api_root + path + self.FORMAT
+        response = self.session.request(method,
+                                        full_uri,
+                                        timeout=self.timeout,
+                                        params=self.api_key)
+        # response.raise_for_status()  # Raise error for bad status
+        return response
 
-	def get_tournaments(self):
-		"""Provides the list of International Soccer tournaments"""
-		path = "tournaments"
-		return self._make_request(path)
+    def get_daily_results(self, year, month, day):
+        """Provides match information and scoring, for all matches
+            played on a given day. The year, month, and day arguments
+            must all be of type int.
+        """
+        path = "schedules/{y:4d}-{m:02d}-{d:02d}/results".format(
+            y=year, m=month, d=day)
+        return self._make_request(path)
 
-	def get_tournament_info(self, tournament_id):
-		"""Provides information for International Soccer tournaments"""
-		path = "tournaments/{_id}/info".format(_id=tournament_id)
-		return self._make_request(path)
+    def get_daily_schedule(self, year, month, day):
+        """Provides match played on a certain day
+            The year, month, and day arguments must all be of type int.
+        """
+        path = "schedules/{y:4d}-{m:02d}-{d:02d}/schedule".format(
+            y=year, m=month, d=day)
+        return self._make_request(path)
 
-	def get_team_profile(self, team_id):
-		"""Team information, including player roster information"""
-		path = "teams/{_id}/profile".format(_id=team_id)
-		return self._make_request(path)
-	
-	def get_player_profile(self, player_id):
-		"""Provides player information"""
-		path = "players/{_id}/profile.{_format}".format(_id=player_id)
-		return self._make_request(path)                          
-	
+    def get_live_results(self):
+        """Provides results for live matches"""
+        path = "schedules/live/results"
+        return self._make_request(path)
+
+    def get_match_summary(self, match_id):
+        """Provides match summary, including team statistics and lineups"""
+        path = "matches/{_id}/summary".format(_id=match_id)
+        return self._make_request(path)
+
+    def get_match_timeline(self, match_id):
+        """Provides match information and scoring, including team
+        statistics and a play-by-play
+        """
+        path = "matches/{_id}/timeline".format(_id=match_id)
+        return self._make_request(path)
+
+    def get_match_lineups(self, match_id):
+        """Provides lineups for a given match"""
+        path = "matches/{_id}/lineups".format(_id=match_id)
+        return self._make_request(path)
+
+    def get_match_probabilities(self, match_id):
+        """Provides match probabilities"""
+        path = "matches/{_id}/probabilities".format(_id=match_id)
+        return self._make_request(path)
+
+    def get_match_fun_facts(self, match_id):
+        """Provides match fun facts"""
+        path = "matches/{_id}/funfacts".format(_id=match_id)
+        return self._make_request(path)
+
+    def get_match_fun_facts(self, match_id):
+        """Provides match fun facts"""
+        path = "matches/{_id}/funfacts".format(_id=match_id)
+        return self._make_request(path)
+
+    def get_missing_players(self, tournament_id):
+        """Provides a listing of players by team who are missing from play"""
+        path = "tournaments/{_id}/missing_players".format(_id=tournament_id)
+        return self._make_request(path)
+
+    def get_player_mapping(self, mapping):
+        """Displays Player and Team IDs from the Soccer v2 API and
+            the converted IDs that are used in the Soccer v3 API.
+        """
+        path = "{mapping}/v2_v3_id_mappings".format(mapping=mapping)
+        return self._make_request(path)
+
+    def get_player_profile(self, player_id):
+        """Provides player information"""
+        path = "players/{_id}/profile".format(_id=player_id)
+        return self._make_request(path)
+
+    def get_player_rankings(self, tournament_id):
+        """Provides player rankings for goals, assists, points, cards,
+        and own goals in a tournament
+        """
+        path = "tournaments/{_id}/leaders".format(_id=tournament_id)
+        return self._make_request(path)
+
+    def get_team_profile(self, team_id):
+        """Team information, including player roster information"""
+        path = "teams/{_id}/profile".format(_id=team_id)
+        return self._make_request(path)
+
+    def get_team_statistics(self, tournament_id, team_id):
+        """Team information, including player roster information"""
+        path = "tournaments/{tournament_id}/teams/{team_id}/statistics".format(
+            tournament_id=tournament_id, team_id=team_id)
+        return self._make_request(path)
+
+    def get_head2head(self, team_id_1, team_id_2):
+        """Provides information on team versus team results"""
+        path = "teams/{_id_1}/versus/{_id_2}/matches".format(_id_1=team_id_1, _id_2=team_id_2)
+        return self._make_request(path)
+
+    def get_tournaments(self):
+        """Provides the list of International Soccer tournaments"""
+        path = "tournaments"
+        return self._make_request(path)
+
+    def get_tournament_info(self, tournament_id):
+        """Provides information for International Soccer tournaments"""
+        path = "tournaments/{_id}/info".format(_id=tournament_id)
+        return self._make_request(path)
--- tests/test_sportradar.py
+++ tests/test_sportradar.py
@@ -5,18 +5,109 @@ import sportradar as sr
 # Import API keys from environment variables
 api_key_name = "SPORTRADAR_API_KEY_SOCCER"
 api_key = os.environ.get(api_key_name, None)
-assert api_key is not None, "Must declare environment variable: {key_name}".format(key_name="SPORTRADAR_API_SOCCER_KEY")
-api = sr.API(api_key)
+assert api_key is not None, "Must declare environment variable: {key_name}".format(
+        key_name="SPORTRADAR_API_SOCCER_KEY")
+api = sr.API(api_key, format_='json', language='en', timeout=5, sleep_time=1.5)
+
 
 class TestAPI(unittest.TestCase):
 
-	@classmethod
-	def setUpClass(cls):
-		print("\n---------------------\nSetting up Sportradar API tests...\n")
-		cls.auth = api_key
-		cls.api = api						
-		cls.response = cls.api.get_tournaments()
+    @classmethod
+    def setUpClass(cls):
+        print("\n---------------------\nSetting up Sportradar API tests...\n")
+        cls.auth = api_key
+        cls.api = api
 
-	def test_valid_status(self):
-		msg = "Response status is not 200."
-		self.assertEqual(self.response.status_code, 200, msg)
+    # How do I write proper tests for these?
+    # I assume just checking status code isn't good enough.
+
+    def test_get_daily_results(self):
+        """Test the daily results GET query"""
+        msg = "Response status is not 200."
+        response = self.api.get_daily_results(2018, 6, 20)
+        self.assertEqual(response.status_code, 200, msg)
+
+    def test_get_daily_schedule(self):
+        """Test the daily schedule GET query"""
+        msg = "Response status is not 200."
+        response = self.api.get_daily_schedule(2018, 6, 20)
+        self.assertEqual(response.status_code, 200, msg)
+
+    # def test_get_live_results(self):
+    #     """Test the live results GET query"""
+    #     # Requires a game in progress
+    #     msg = "Response status is not 200."
+    #     response = self.api.get_live_results()
+    #     self.assertEqual(response.status_code, 200, msg)
+
+    def test_get_match_summary(self):
+        """Test the match summary GET query"""
+        msg = "Response status is not 200."
+        response = self.api.get_match_summary("sr:match:7696036")
+        self.assertEqual(response.status_code, 200, msg)
+
+    def test_get_match_timeline(self):
+        """Test the match timeline GET query"""
+        msg = "Response status is not 200."
+        response = self.api.get_match_timeline("sr:match:7696036")
+        self.assertEqual(response.status_code, 200, msg)
+
+    def test_get_match_lineups(self):
+        """Test the match lineups GET query"""
+        msg = "Response status is not 200."
+        response = self.api.get_match_lineups("sr:match:7696036")
+        self.assertEqual(response.status_code, 200, msg)
+
+    def test_get_match_probabilities(self):
+        """Test the match probabilities GET query"""
+        msg = "Response status is not 200."
+        response = self.api.get_match_probabilities("sr:match:7696036")
+        self.assertEqual(response.status_code, 200, msg)
+
+    # def test_get_match_fun_facts(self):
+    # # *** API RETURNS 404 IN THEIR EXAMPLE ***
+    #     """Test the match fun facts GET query"""
+    #     msg = "Response status is not 200."
+    #     response = self.api.get_match_fun_facts("sr:match:10987366")
+    #     self.assertEqual(response.status_code, 200, msg)
+
+    def test_get_missing_players(self):
+        msg = "Response status is not 200."
+        response = self.api.get_missing_players("sr:tournament:1")
+        self.assertEqual(response.status_code, 200, msg)
+
+    def test_get_player_mapping(self):
+        msg = "Response status is not 200."
+        response = self.api.get_player_mapping("teams")
+        self.assertEqual(response.status_code, 200, msg)
+
+    def test_get_player_profile(self):
+        msg = "Response status is not 200."
+        response = self.api.get_player_profile("sr:player:149304")
+        self.assertEqual(response.status_code, 200, msg)
+
+    def test_get_player_rankings(self):
+        msg = "Response status is not 200."
+        response = self.api.get_player_rankings("sr:tournament:1")
+        self.assertEqual(response.status_code, 200, msg)
+
+    def test_get_team_profile(self):
+        msg = "Response status is not 200."
+        response = self.api.get_team_profile("sr:competitor:4715")
+        self.assertEqual(response.status_code, 200, msg)
+
+    def test_get_team_statistics(self):
+        msg = "Response status is not 200."
+        response = self.api.get_team_statistics("sr:tournament:1", "sr:competitor:4698")
+        self.assertEqual(response.status_code, 200, msg)
+
+    def test_get_head2head(self):
+        msg = "Response status is not 200."
+        response = self.api.get_head2head("sr:competitor:4715", "sr:competitor:4698")
+        self.assertEqual(response.status_code, 200, msg)
+
+    def test_get_tournaments(self):
+        """Test the tournaments GET query"""
+        msg = "Response status is not 200."
+        response = self.api.get_tournaments()
+        self.assertEqual(response.status_code, 200, msg)
