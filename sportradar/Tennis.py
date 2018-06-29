4f08fb79c1964dda89586935a8e87b44280a9b6e
johnwmillr <john.w.millr@gmail.com>
2018-06-29 00:03:52 -0400

    Add support for Tennis API

---
+++ sportradar/Tennis.py
@@ -0,0 +1,168 @@
+# Sportradar APIs
+# Copyright 2018 John W. Miller
+# See LICENSE for details.
+
+from sportradar.api import API
+
+
+class Tennis(API):
+
+    def __init__(self, api_key, format_='json', language='en', timeout=5, sleep_time=1.5):
+        super().__init__(api_key, format_, language, timeout, sleep_time)
+
+    def get_daily_results(self, year, month, day):
+        """Provides match information and scoring, for all matches played on a given da
+            y
+        """
+        path = "tennis-t2/en/schedules/{year:4d}-{month:02d}-{day:02d}/results".format(
+            year=year, month=month, day=day)
+        print(path)
+        return self._make_request(path)
+
+    def get_daily_schedule(self, year, month, day):
+        """Provides the schedule for a given day"""
+        path = "tennis-t2/en/schedules/{year:4d}-{month:02d}-{day:02d}/schedule".format(
+            year=year, month=month, day=day)
+        print(path)
+        return self._make_request(path)
+
+    def get_deleted_matches(self):
+        """Provides deleted matches"""
+        path = "tennis-t2/en/schedules/deleted_matches".format()
+        print(path)
+        return self._make_request(path)
+
+    def get_match_probabilities(self, match_id):
+        """Provides match probabilities"""
+        path = "tennis-t2/en/matches/{match_id}/probabilities".format(
+            match_id=match_id)
+        print(path)
+        return self._make_request(path)
+
+    def get_match_summary(self, match_id):
+        """Provides match summary, including scores and statistics"""
+        path = "tennis-t2/en/matches/{match_id}/summary".format(match_id=match_id)
+        print(path)
+        return self._make_request(path)
+
+    def get_match_timeline(self, match_id):
+        """Provides match information and scoring"""
+        path = "tennis-t2/en/matches/{match_id}/timeline".format(match_id=match_id)
+        print(path)
+        return self._make_request(path)
+
+    def get_player_profile(self, player_id):
+        """Provides player information"""
+        path = "tennis-t2/en/players/{player_id}/profile".format(player_id=player_id)
+        print(path)
+        return self._make_request(path)
+
+    def get_player_versus_player(self, player_id_1, player_id_2):
+        """Provides information on team versus team results"""
+        path = "tennis-t2/en/players/{player_id_1}/versus/{player_id_2}/matches".format(
+            player_id_1=player_id_1, player_id_2=player_id_2)
+        print(path)
+        return self._make_request(path)
+
+    def get_player_results(self, player_id):
+        """Provides the results for a given player"""
+        path = "tennis-t2/en/players/{player_id}/results".format(player_id=player_id)
+        print(path)
+        return self._make_request(path)
+
+    def get_player_schedule(self, player_id):
+        """Provides the schedule for a player"""
+        path = "tennis-t2/en/players/{player_id}/schedule".format(player_id=player_id)
+        print(path)
+        return self._make_request(path)
+
+    def get_player_rankings(self):
+        """Provides player rankings for a given tournaments"""
+        path = "tennis-t2/en/players/rankings".format()
+        print(path)
+        return self._make_request(path)
+
+    def get_player_race_rankings(self):
+        """Provides player rankings for a given tournaments"""
+        path = "tennis-t2/en/players/race_rankings".format()
+        print(path)
+        return self._make_request(path)
+
+    def get_double_team_profile(self, team_id):
+        """Team information, including player roster information"""
+        path = "tennis-t2/en/double_teams/{team_id}/profile".format(team_id=team_id)
+        print(path)
+        return self._make_request(path)
+
+    def get_double_team_results(self, team_id):
+        """Provides a Team's Results"""
+        path = "tennis-t2/en/double_teams/{team_id}/results".format(team_id=team_id)
+        print(path)
+        return self._make_request(path)
+
+    def get_double_team_schedule(self, team_id):
+        """Provides a Team's Schedule"""
+        path = "tennis-t2/en/double_teams/{team_id}/schedule".format(team_id=team_id)
+        print(path)
+        return self._make_request(path)
+
+    def get_double_team_versus_team(self, team_id_1, team_id_2):
+        """Provides information on team versus team results"""
+        path = "tennis-t2/en/double_teams/{team_id_1}/versus/{team_id_2}/matches".format(
+            team_id_1=team_id_1, team_id_2=team_id_2)
+        print(path)
+        return self._make_request(path)
+
+    def get_doubles_rankings(self):
+        """Provides rankings for a Doubles"""
+        path = "tennis-t2/en/double_teams/rankings".format()
+        print(path)
+        return self._make_request(path)
+
+    def get_doubles_race_rankings(self):
+        """Provides double rankings for a Doubles"""
+        path = "tennis-t2/en/double_teams/race_rankings".format()
+        print(path)
+        return self._make_request(path)
+
+    def get_tournament_info(self, tournament_id):
+        """Provide information for a given Tournament found in the Tournament List
+            endpoint
+        """
+        path = "tennis-t2/en/tournaments/{tournament_id}/info".format(
+            tournament_id=tournament_id)
+        print(path)
+        return self._make_request(path)
+
+    def get_tournament_list(self):
+        """Provides a list of all covered Tournaments"""
+        path = "tennis-t2/en/tournaments".format()
+        print(path)
+        return self._make_request(path)
+
+    def get_tournament_ongoing(self):
+        """Provides updated information for a given Tournament found in the Tournament
+            List endpoint
+        """
+        path = "tennis-t2/en/tournaments/ongoing".format()
+        print(path)
+        return self._make_request(path)
+
+    def get_tournament_results(self, tournament_id):
+        """Provides results for a given Tournament found in the Tournament List en
+            dpoint
+        """
+        path = "tennis-t2/en/tournaments/{tournament_id}/results".format(
+            tournament_id=tournament_id)
+        print(path)
+        return self._make_request(path)
+
+    def get_tournament_schedule(self, tournament_id):
+        """Provides the schedule for a given Tournament found in the Tournament List
+            endpoint
+        """
+        path = "tennis-t2/en/tournaments/{tournament_id}/schedule".format(
+            tournament_id=tournament_id)
+        print(path)
+        return self._make_request(path)
+
---
+++ tests/test_Tennis.py
@@ -0,0 +1,169 @@
+import os
+import unittest
+from sportradar import Tennis
+
+# Import API keys from environment variables
+api_key_name = "SPORTRADAR_API_KEY_TENNIS"
+api_key = os.environ.get(api_key_name, None)
+assert api_key is not None, "Must declare environment variable: {key_name}".format(
+    key_name=api_key_name)
+api = Tennis.Tennis(api_key, format_="json", language="en",
+    timeout=5, sleep_time=2)
+
+
+class TestAPI(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        print("\n---------------------\nSetting up {} tests...\n".format("Tennis"))
+        cls.auth = api_key
+        cls.api = api
+        cls.year = 2018
+        cls.month = 6
+        cls.day = 1
+        cls.match_id = "sr:match:10134403"
+        cls.player_id = "sr:competitor:19578"
+        cls.player_id_1 = "sr:competitor:19578"
+        cls.player_id_2 = "sr:competitor:18254"
+        cls.tournament_id = "sr:tournament:2591"
+        cls.team_id = "sr:competitor:298214"
+        cls.team_id_1 = "sr:competitor:257813"
+        cls.team_id_2 = "sr:competitor:187839"
+
+    def test_get_daily_results(self):
+        """Test the daily results GET query"""
+        msg = "Response status is not 200"
+        response = self.api.get_daily_results(self.year, self.month, self.day)
+        self.assertEqual(response.status_code, 200, msg)
+
+    def test_get_daily_schedule(self):
+        """Test the daily schedule GET query"""
+        msg = "Response status is not 200"
+        response = self.api.get_daily_schedule(self.year, self.month, self.day)
+        self.assertEqual(response.status_code, 200, msg)
+
+    def test_get_deleted_matches(self):
+        """Test the deleted matches GET query"""
+        msg = "Response status is not 200"
+        response = self.api.get_deleted_matches()
+        self.assertEqual(response.status_code, 200, msg)
+
+    def test_get_match_probabilities(self):
+        """Test the match probabilities GET query"""
+        msg = "Response status is not 200"
+        response = self.api.get_match_probabilities(self.match_id)
+        self.assertEqual(response.status_code, 200, msg)
+
+    def test_get_match_summary(self):
+        """Test the match summary GET query"""
+        msg = "Response status is not 200"
+        response = self.api.get_match_summary(self.match_id)
+        self.assertEqual(response.status_code, 200, msg)
+
+    def test_get_match_timeline(self):
+        """Test the match timeline GET query"""
+        msg = "Response status is not 200"
+        response = self.api.get_match_timeline(self.match_id)
+        self.assertEqual(response.status_code, 200, msg)
+
+    def test_get_player_profile(self):
+        """Test the player profile GET query"""
+        msg = "Response status is not 200"
+        response = self.api.get_player_profile(self.player_id)
+        self.assertEqual(response.status_code, 200, msg)
+
+    def test_get_player_versus_player(self):
+        """Test the player versus player GET query"""
+        msg = "Response status is not 200"
+        response = self.api.get_player_versus_player(self.player_id_1, self.player_id_2)
+        self.assertEqual(response.status_code, 200, msg)
+
+    def test_get_player_results(self):
+        """Test the player results GET query"""
+        msg = "Response status is not 200"
+        response = self.api.get_player_results(self.player_id)
+        self.assertEqual(response.status_code, 200, msg)
+
+    def test_get_player_schedule(self):
+        """Test the player schedule GET query"""
+        msg = "Response status is not 200"
+        response = self.api.get_player_schedule(self.player_id)
+        self.assertEqual(response.status_code, 200, msg)
+
+    def test_get_player_rankings(self):
+        """Test the player rankings GET query"""
+        msg = "Response status is not 200"
+        response = self.api.get_player_rankings()
+        self.assertEqual(response.status_code, 200, msg)
+
+    def test_get_player_race_rankings(self):
+        """Test the player race rankings GET query"""
+        msg = "Response status is not 200"
+        response = self.api.get_player_race_rankings()
+        self.assertEqual(response.status_code, 200, msg)
+
+    def test_get_double_team_profile(self):
+        """Test the double-team profile GET query"""
+        msg = "Response status is not 200"
+        response = self.api.get_double_team_profile(self.team_id)
+        self.assertEqual(response.status_code, 200, msg)
+
+    def test_get_double_team_results(self):
+        """Test the double-team results GET query"""
+        msg = "Response status is not 200"
+        response = self.api.get_double_team_results(self.team_id)
+        self.assertEqual(response.status_code, 200, msg)
+
+    def test_get_double_team_schedule(self):
+        """Test the double-team schedule GET query"""
+        msg = "Response status is not 200"
+        response = self.api.get_double_team_schedule(self.team_id)
+        self.assertEqual(response.status_code, 200, msg)
+
+    def test_get_double_team_versus_team(self):
+        """Test the double team versus team GET query"""
+        msg = "Response status is not 200"
+        response = self.api.get_double_team_versus_team(self.team_id_1, self.team_id_2)
+        self.assertEqual(response.status_code, 200, msg)
+
+    def test_get_doubles_rankings(self):
+        """Test the doubles rankings GET query"""
+        msg = "Response status is not 200"
+        response = self.api.get_doubles_rankings()
+        self.assertEqual(response.status_code, 200, msg)
+
+    def test_get_doubles_race_rankings(self):
+        """Test the doubles race rankings GET query"""
+        msg = "Response status is not 200"
+        response = self.api.get_doubles_race_rankings()
+        self.assertEqual(response.status_code, 200, msg)
+
+    def test_get_tournament_info(self):
+        """Test the tournament info GET query"""
+        msg = "Response status is not 200"
+        response = self.api.get_tournament_info(self.tournament_id)
+        self.assertEqual(response.status_code, 200, msg)
+
+    def test_get_tournament_list(self):
+        """Test the tournament list GET query"""
+        msg = "Response status is not 200"
+        response = self.api.get_tournament_list()
+        self.assertEqual(response.status_code, 200, msg)
+
+    def test_get_tournament_ongoing(self):
+        """Test the tournament ongoing GET query"""
+        msg = "Response status is not 200"
+        response = self.api.get_tournament_ongoing()
+        self.assertEqual(response.status_code, 200, msg)
+
+    def test_get_tournament_results(self):
+        """Test the tournament results GET query"""
+        msg = "Response status is not 200"
+        response = self.api.get_tournament_results(self.tournament_id)
+        self.assertEqual(response.status_code, 200, msg)
+
+    def test_get_tournament_schedule(self):
+        """Test the tournament schedule GET query"""
+        msg = "Response status is not 200"
+        response = self.api.get_tournament_schedule(self.tournament_id)
+        self.assertEqual(response.status_code, 200, msg)
