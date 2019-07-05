86cf95257d60c9090e59cb61b32a9e6e2bbff972
johnwmillr <john.w.millr@gmail.com>
2019-07-05 13:28:23 -0700

    Update version to 1.3.2

--- sportradar/__init__.py
+++ sportradar/__init__.py
@@ -5,7 +5,7 @@
 """
 Sportradar APIs library
 """
-__version__ = '1.3.1'
+__version__ = '1.3.2'
 __author__ = 'John W. Miller'
 __description__ = 'A Python wrapper for the Sportradar APIs'
 __license__ = 'MIT'
