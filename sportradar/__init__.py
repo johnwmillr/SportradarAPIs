3f5679fd573bf3ce5c077dc63628548a9a7b653b
John Miller <john.w.millr@gmail.com>
2021-01-12 19:23:44 -0800

    Update from 1.5.0 to 1.6.0

--- sportradar/__init__.py
+++ sportradar/__init__.py
@@ -1,3 +1,4 @@
+
 # Sportradar APIs
 # Copyright 2018 John W. Miller
 # See LICENSE for details.
@@ -5,7 +6,7 @@
 """
 Sportradar APIs library
 """
-__version__ = '1.5.0'
+__version__ = '1.6.0'
 __author__ = 'John W. Miller'
 __description__ = 'A Python wrapper for the Sportradar APIs'
 __license__ = 'MIT'
