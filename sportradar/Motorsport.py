ce16d5b644639bef9f21c5f0037833a404e41e15
Teresa Fontanella De Santis <tfontanella011@gmail.com>
2021-06-26 19:36:36 -0300

    Add Motorsport documentation

--- sportradar/Motorsport.py
+++ sportradar/Motorsport.py
@@ -7,6 +7,7 @@ from sportradar.api import API
 
 
 class Motorsport(API):
+    """Motorsport API"""
 
     def __init__(self, api_key, access_level='t', language='en', format_='json',
         timeout=5, sleep_time=1.5):
@@ -21,6 +22,8 @@ class Motorsport(API):
 
     def competitor_profile(self, competitor_id):
         """Obtain competitor profile for Motorcycle.
+
+        :param competitor_id: id of a given competitor
         """
         path = "{prefix}/competitors/{competitor_id}/profile".format(
             prefix=self.api_prefix, competitor_id=competitor_id)
@@ -28,6 +31,8 @@ class Motorsport(API):
 
     def competitor_results(self, competitor_id):
         """Obtain competitor results for Motorcycle.
+
+        :param competitor_id: id of a given competitor.
         """
         path = "{prefix}/competitors/{competitor_id}/results".format(
             prefix=self.api_prefix, competitor_id=competitor_id)
@@ -35,89 +40,115 @@ class Motorsport(API):
 
     def competitor_schedule(self, competitor_id):
         """Obtain competitor schedule for Motorcycle.
+
+        :param competitor_id: id of a given competitor.
         """
         path = "{prefix}/competitors/{competitor_id}/schedule".format(
             prefix=self.api_prefix, competitor_id=competitor_id)
         return self._make_request(path)
 
     def daily_results(self, year, month, day):
-        """
+        """Obtain daily results.
+
+        :param year: year int. Must be greater or equal than 2014.
+        :param month: month int. Must be between 1 and 12.
+        :param day: day int.
         """
         path = "{prefix}/schedules/{year:4d}-{month:02d}-{day:02d}/results".format(
             prefix=self.api_prefix, year=year, month=month, day=day)
         return self._make_request(path)
 
     def daily_schedule(self, year, month, day):
-        """
+        """Obtain daily schedule.
+
+        :param year: year int. Must be greater or equal than 2014.
+        :param month: month int. Must be between 1 and 12.
+        :param day: day int.
         """
         path = "{prefix}/schedules/{year:4d}-{month:02d}-{day:02d}/schedule".format(
             prefix=self.api_prefix, year=year, month=month, day=day)
         return self._make_request(path)
 
     def head_to_head(self, competitor_id, competitor_id2):
-        """
+        """Obtain comparisson between two Motorcycle competitors.
+
+        :param competitor_id: id of a given competitor.
+        :param competitor_id2: id of another given competitor.
         """
         path = "{prefix}/competitors/{competitor_id}/versus/{competitor_id2}/matches".format(
             prefix=self.api_prefix, competitor_id=competitor_id, competitor_id2=competitor_id2)
         return self._make_request(path)
 
     def seasons(self, tournament_or_season_id):
-        """
-        """
+        """Obtain seasons of a given Motorcycle tournament."""
         path = "{prefix}/tournaments/{tournament_or_season_id}/seasons".format(
             prefix=self.api_prefix, tournament_or_season_id=tournament_or_season_id)
         return self._make_request(path)
 
     def sport_event_probabilities(self, match_id):
-        """
+        """Obtain sport event probabilities for a given Motrcycle match.
+
+        :param match_id: id of a given match.
         """
         path = "{prefix}/matches/{match_id}/probabilities".format(
             prefix=self.api_prefix, match_id=match_id)
         return self._make_request(path)
 
     def sport_event_timeline(self, match_id):
-        """
+        """Obtain sport event timeline for a given Motrcycle match.
+
+        :param match_id: id of a given match.
         """
         path = "{prefix}/sport_events/{match_id}/timeline".format(
             prefix=self.api_prefix, match_id=match_id)
         return self._make_request(path)
 
     def tournament_info(self, tournament_or_season_id):
-        """
+        """Obtain information of a given Motorcycle Tournament.
+
+        :param tournament_or_season_id: id of a given tournament or season.
         """
         path = "{prefix}/tournaments/{tournament_or_season_id}/info".format(
             prefix=self.api_prefix, tournament_or_season_id=tournament_or_season_id)
         return self._make_request(path)
 
     def tournament_list(self):
-        """
+        """Obtain Motorcycle Tournament list.
         """
         path = "{prefix}/tournaments".format(prefix=self.api_prefix)
         return self._make_request(path)
 
     def tournament_live_standings(self, tournament_or_season_id):
-        """
+        """Obtain live standings of a given Motorcycle Tournament.
+
+        :param tournament_or_season_id: id of a given tournament or season.
         """
         path = "{prefix}/tournaments/{tournament_or_season_id}/live_standings".format(
             prefix=self.api_prefix, tournament_or_season_id=tournament_or_season_id)
         return self._make_request(path)
 
     def tournament_results(self, tournament_or_season_id):
-        """
+        """Obtain results of a given Motorcycle Tournament.
+
+        :param tournament_or_season_id: id of a given tournament or season.
         """
         path = "{prefix}/tournaments/{tournament_or_season_id}/results".format(
             prefix=self.api_prefix, tournament_or_season_id=tournament_or_season_id)
         return self._make_request(path)
 
     def tournament_schedule(self, tournament_or_season_id):
-        """
+        """Obtain schedule of a given Motorcycle Tournament.
+
+        :param tournament_or_season_id: id of a given tournament or season.
         """
         path = "{prefix}/tournaments/{tournament_or_season_id}/schedule".format(
             prefix=self.api_prefix, tournament_or_season_id=tournament_or_season_id)
         return self._make_request(path)
 
     def tournament_standings(self, tournament_or_season_id):
-        """
+        """Obtain standings of a given Motorcycle Tournament.
+
+        :param tournament_or_season_id: id of a given tournament or season.
         """
         path = "{prefix}/tournaments/{tournament_or_season_id}/standings".format(
             prefix=self.api_prefix, tournament_or_season_id=tournament_or_season_id)
