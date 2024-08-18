1b41f032bde69fd093037e8d15db95f61d7e91d1
Michael Adams <madams@techfak.uni-bielefeld.de>
2024-08-18 08:21:42 +0200

    Library bumps, Handball bump

--- sportradar/Handball.py
+++ sportradar/Handball.py
@@ -8,15 +8,25 @@ from sportradar.api import API
 
 class Handball(API):
 
-    def __init__(self, api_key, format_='json', access_level='trial', version="v2",
-                 league='INTL', language='en', timeout=5, sleep_time=1.5):
+    def __init__(
+        self,
+        api_key,
+        format_="json",
+        access_level="trial",
+        version="v2",
+        league="INTL",
+        language="en",
+        timeout=5,
+        sleep_time=1.5,
+    ):
         super().__init__(api_key, format_, timeout, sleep_time)
         self.access_level = access_level
         self.language = language
         self.version = version
         self.league = league.lower()
-        self.prefix = 'handball/{level}/{version}/{lang}/'.format(level=self.access_level,
-                version=self.version,  lang=self.language)
+        self.prefix = "handball/{level}/{version}/{lang}/".format(
+            level=self.access_level, version=self.version, lang=self.language
+        )
 
     def get_competitions(self):
         """Provides list of all competitions scouted by Sportradar."""
@@ -24,26 +34,31 @@ class Handball(API):
         path = "competitions"
 
         return self._make_request(self.prefix + path)
-    
+
     def get_seasons_for_competition(self, competition_id):
         """Provides list of all seasons in a given competition scouted by Sportradar."""
-        
+
         path = "competitions/{_id}/seasons".format(_id=competition_id)
 
         return self._make_request(self.prefix + path)
-    
+
     def get_players_for_season(self, season_id):
         """Provides list of all competitors in a given season."""
         path = "seasons/{_id}/players".format(_id=season_id)
 
         return self._make_request(self.prefix + path)
-    
+
+    def get_team_profile(self, team_id):
+        """Team information, including player roster information"""
+        path = "competitors/{_id}/profile".format(_id=team_id)
+        return self._make_request(self.prefix + path)
+
     def get_competitors_for_season(self, season_id):
         """Provides list of all competitors in a given season."""
         path = "seasons/{_id}/competitors".format(_id=season_id)
 
         return self._make_request(self.prefix + path)
-    
+
     def get_season_summaries(self, season_id, start=None, limit=None):
         """Provides list of all competitors in a given season with optional start and limit parameters."""
         path = "seasons/{_id}/summaries".format(_id=season_id)
@@ -51,18 +66,16 @@ class Handball(API):
         # Prepare parameters dictionary
         params = {}
         if start is not None:
-            params['start'] = start
+            params["start"] = start
         if limit is not None:
-            params['limit'] = limit
+            params["limit"] = limit
 
         return self._make_request(self.prefix + path, params=params)
 
-    
     def get_player_profile(self, player_id):
         """Provides player information"""
         path = "players/{_id}/profile".format(_id=player_id)
-        return self._make_request(self.prefix + path)    
-
+        return self._make_request(self.prefix + path)
 
     def get_sport_event_timeline(self, match_id):
         """Provides list of all competitors in a given season."""
--- tests/test_Handball.py
+++ tests/test_Handball.py
@@ -3,28 +3,39 @@ import unittest
 from sportradar import Handball
 
 # Import API keys from environment variables
-api_key_name = "SPORTRADAR_API_KEY_HANDBALL"
+api_key_name = "API_KEY_SPORTRADAR"
 api_key = os.environ.get(api_key_name, None)
-assert api_key is not None, "Must declare environment variable: {key_name}".format(
-    key_name=api_key_name)
-api = Handball.Handball(api_key, format_='json', language='en', timeout=5, sleep_time=2)
+assert (
+    api_key is not None
+), "Must declare environment variable: {key_name}".format(
+    key_name=api_key_name
+)
+api = Handball.Handball(
+    api_key, format_="json", language="en", timeout=5, sleep_time=2
+)
 
 
 class TestAPI(unittest.TestCase):
 
     @classmethod
     def setUpClass(cls):
-        print("\n---------------------\nSetting up {} tests...\n".format("Handball"))
+        print(
+            "\n---------------------\nSetting up {} tests...\n".format(
+                "Handball"
+            )
+        )
         cls.auth = api_key
         cls.api = api
-        cls.competition_id = "sr:competition:149" # German bundesliga
-        cls.season_id = "sr:season:95685" # German Bundesliga 22/23
-        cls.sport_event_id = "sr:sport_event:34738527" # TBV vs. VFL on the 01.09.2022
-        cls.team_id = "sr:competitor:3996" # TBV Lemgo Lippe
-        cls.team_id_2 = "sr:competitor:3994" # VFL Gummersbach
-        cls.player_id = "sr:player:918114" # Zerbe Lukas
-        cls.team_id = "sr:competitor:3996" # TBV Lemgo Lippe
-        cls.team_id_2 = "sr:competitor:3994" # VFL Gummersbach
+        cls.competition_id = "sr:competition:149"  # German bundesliga
+        cls.season_id = "sr:season:95685"  # German Bundesliga 22/23
+        cls.sport_event_id = (
+            "sr:sport_event:34738527"  # TBV vs. VFL on the 01.09.2022
+        )
+        cls.team_id = "sr:competitor:3996"  # TBV Lemgo Lippe
+        cls.team_id_2 = "sr:competitor:3994"  # VFL Gummersbach
+        cls.player_id = "sr:player:918114"  # Zerbe Lukas
+        cls.team_id = "sr:competitor:3996"  # TBV Lemgo Lippe
+        cls.team_id_2 = "sr:competitor:3994"  # VFL Gummersbach
 
     # How do I write proper tests for these?
     # I assume just checking status code isn't good enough.
@@ -40,7 +51,7 @@ class TestAPI(unittest.TestCase):
         msg = "Response status is not 200."
         response = self.api.get_seasons_for_competition(self.competition_id)
         self.assertEqual(response.status_code, 200, msg)
-        
+
     def test_get_players_for_season(self):
         """Test the match summary GET query"""
         msg = "Response status is not 200."
