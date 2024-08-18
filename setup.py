6446e106894873e702951852b8d25359e692ca16
Michael Adams <madams@techfak.uni-bielefeld.de>
2024-08-18 08:23:54 +0200

    Library bumps, Handball bump

--- setup.py
+++ setup.py
@@ -15,27 +15,30 @@ if mo:
     version = mo.group(1)
 else:
     raise RuntimeError(
-        "Unable to find version string in {}".format(VERSIONFILE))
+        "Unable to find version string in {}".format(VERSIONFILE)
+    )
 
 here = os.path.abspath(os.path.dirname(__file__))
-with open(os.path.join(here, 'README.md')) as f:
+with open(os.path.join(here, "README.md")) as f:
     README = f.read()
 
-setup(name="sportradar",
-      version=version,
-      description="A Python wrapper for the Sportradar APIs",
-      long_description=README,
-      long_description_content_type="text/markdown",
-      license="MIT",
-      author="John W. Miller",
-      url="https://github.com/johnwmillr/SportradarAPIs",
-      packages=find_packages(exclude=['tests']),
-      install_requires=["requests"],
-      keywords="sportsradar API sports basketball NBA football NFL soccer",
-      python_requires=">=3.*",
-      classifiers=[
-              'Topic :: Software Development :: Libraries',
-          'Operating System :: OS Independent',
-          'Programming Language :: Python',
-          'Programming Language :: Python :: 3',
-      ])
+setup(
+    name="sportradar",
+    version=version,
+    description="A Python wrapper for the Sportradar APIs",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    license="MIT",
+    author="John W. Miller",
+    url="https://github.com/johnwmillr/SportradarAPIs",
+    packages=find_packages(exclude=["tests"]),
+    install_requires=["requests"],
+    keywords="sportsradar API sports basketball NBA football NFL soccer",
+    # python_requires=">=3.*",
+    classifiers=[
+        "Topic :: Software Development :: Libraries",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+    ],
+)
